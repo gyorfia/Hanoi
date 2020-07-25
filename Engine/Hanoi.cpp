dd0e2482c25993734562242877c22bdfb764f796
GAdamY <gyorfia9@gmail.com>
2020-07-25 23:27:25 +0200

    bicolor modes

--- Engine/Hanoi.cpp
+++ Engine/Hanoi.cpp
@@ -10,34 +10,52 @@ Hanoi::Hanoi(Graphics& gfx)
 		pegs[i].index = i;
 		pegSX += int (Graphics::ScreenWidth / 3);
 	}
-	//assign diskpositions for NormHanoi
-	for (int j = 0; j < nDisks; j++)
-	{
-		pegs[0].highestPegI.push_back(j);
-	}
-	//colors for NormHanoi
-		std::random_device rd;
-		std::mt19937 rng(rd());
-		std::uniform_int_distribution <int> rCol{ 60, 220 };
-		int cC = int(200 / (nDisks - 1));
 	//biggest disk size
 	const int firstW = int(Graphics::ScreenWidth / 6 - 1 - gap);
 	const int firstH = int((Graphics::ScreenHeight - topGap + (ceil(nDisks / 2) * nDisks) * heightChange) / nDisks);
-
+	const int firstBiCH = int((Graphics::ScreenHeight - topGap + ((ceil(nDisks / 2) * nDisks) * heightChange) / 2) / nDisks);
 	assert(0 < firstW - (nDisks - 1) * widthChange);
 	assert(0 < firstH - (nDisks - 1) * heightChange);
-	//disk size and color assigning for NormHanoi
-	for (int i = 0; i < nDisks; i++)
+	assert(0 < firstBiCH - (nDisks / 2 - 1) * heightChange);
+	//mode select
+	if (normHanoi)
 	{
-		Color c;
-		if(randomColors)
-		c = Color(rCol(rng), rCol(rng), rCol(rng));
-		else
-		c = Color(55 + cC * i, 0, 0);
-		disks[i].disk = Rect(0, 0, firstW - i * widthChange, firstH - i * heightChange, c);
+		SetupNormHanoi(firstW, firstH);
+		NormalHanoi(nDisks, pegs[0], pegs[1], pegs[2]);
 	}
-	//logic
-	NormalHanoi(nDisks, pegs[0], pegs[1], pegs[2]);
+	else if (doubleHanoi)
+	{
+		SetupDoubleHanoi();
+		EnhancedDoubleHanoi(nDisks / 2, pegs[0], pegs[1], pegs[2]);
+	}
+	else if (mergeHanoi)
+	{
+		SetupMergeHanoi();
+		MergeHanoi(nDisks / 2, pegs[0], pegs[1], pegs[2]);
+	}
+	else if (splitHanoi)
+	{
+		SetupSplitHanoi();
+		SplitHanoi(nDisks / 2, pegs[0], pegs[1], pegs[2]);
+	}
+	else if (baseSwapHanoi)
+	{
+		SetupBaseSwapHanoi();
+		BaseSwapHanoi(nDisks / 2, pegs[0], pegs[1], pegs[2]);
+	}
+	else if (easyBicolorHanoi)
+	{
+		SetupEasyBicolorHanoi();
+		EasyBicolorHanoi(nDisks / 2, pegs[0], pegs[1], pegs[2]);
+	}
+	else if (bicolorHanoi)
+	{
+		SetupBicolorHanoi();
+		BicolorHanoi(nDisks / 2, pegs[0], pegs[1], pegs[2]);
+	}
+
+	if (!normHanoi)
+		BicolorDiskSetup(firstW, firstBiCH);
 	LoadStep();
 }
 
@@ -61,6 +79,202 @@ void Hanoi::NormalHanoi(int n, Peg& A, Peg& B, Peg& Via)
 	}
 }
 
+void Hanoi::EnhancedDoubleHanoi(int n, Peg& A, Peg& B, Peg& Via)
+{
+	DoubleHanoi(n, A, Via, B);
+	DoubleHanoi(n, Via, B, A);
+}
+
+void Hanoi::DoubleHanoi(int n, Peg& A, Peg& B, Peg& Via)
+{
+	if (n == 1)
+	{
+		FromAToB(A, B);
+		FromAToB(A, B);
+	}
+	else
+	{
+		DoubleHanoi(n - 1, A, Via, B);
+		FromAToB(A, B);
+		FromAToB(A, B);
+		DoubleHanoi(n - 1, Via, B, A);
+	}
+}
+
+void Hanoi::MergeHanoi(int n, Peg& A, Peg& B, Peg& Via)
+{
+	if (n == 1)
+	{
+		FromAToB(B, A);
+	}
+	else
+	{
+		MergeHanoi(n - 1, A, B, Via);
+		DoubleHanoi(n - 1, A, Via, B);
+		FromAToB(B, A);
+		DoubleHanoi(n - 1, Via, A, B);
+	}
+}
+
+void Hanoi::SplitHanoi(int n, Peg& A, Peg& B, Peg& Via)
+{
+	if (n == 1)
+	{
+		FromAToB(A, B);
+	}
+	else
+	{
+		DoubleHanoi(n - 1, A, Via, B);
+		FromAToB(A, B);
+		DoubleHanoi(n - 1, Via, A, B);
+		SplitHanoi(n - 1, A, B, Via);
+	}
+}
+
+void Hanoi::BaseSwapHanoi(int n, Peg& A, Peg& B, Peg& Via)
+{
+	if (n == 1)
+	{
+		FromAToB(A, Via);
+		FromAToB(B, A);
+		FromAToB(Via, B);
+	}
+	else
+	{
+		MergeHanoi(n - 1, A, B, Via);
+		FromAToB(B, Via);
+		DoubleHanoi(n - 1, A, Via, B);
+		FromAToB(A, B);
+		DoubleHanoi(n - 1, Via, B, A);
+		FromAToB(Via, A);
+		EnhancedDoubleHanoi(n - 1, B, A, Via);
+		SplitHanoi(n - 1, A, B, Via);
+	}
+}
+
+void Hanoi::EasyBicolorHanoi(int n, Peg& A, Peg& B, Peg& Via)
+{
+	if(n == 1){}
+	else
+	{
+		BicolorHanoi(n - 1, A, B, Via);
+	}
+}
+
+void Hanoi::BicolorHanoi(int n, Peg& A, Peg& B, Peg& Via)
+{
+	if (n == 1)
+	{
+		FromAToB(A, Via);
+		FromAToB(B, A);
+		FromAToB(Via, B);
+	}
+	else
+	{
+		EasyBicolorHanoi(n - 1, A, B, Via);
+		BaseSwapHanoi(n, A, B, Via);
+	}
+}
+
+void Hanoi::BicolorDiskSetup(const int firstW, const int firstH)
+{
+	Color c[2] = {Colors::Blue, Colors::Red};
+	for (int i = 0; i < nDisks; i++)
+	{
+		disks[i].disk = Rect(0, 0, firstW - i * widthChange, firstH - i * heightChange, c[0]);
+		i++;
+		disks[i].disk = Rect(0, 0, firstW - (i-1) * widthChange, firstH - (i - 1) * heightChange, c[1]);
+	}
+}
+
+void Hanoi::SetupNormHanoi(const int firstW, const int firstH)
+{
+	//assign diskpositions for NormHanoi
+	for (int j = 0; j < nDisks; j++)
+	{
+		pegs[0].highestPegI.push_back(j);
+	}
+	//colors for NormHanoi
+	std::random_device rd;
+	std::mt19937 rng(rd());
+	std::uniform_int_distribution <int> rCol{ 60, 220 };
+	int cC = int(200 / (nDisks - 1));
+
+	//disk size and color assigning for NormHanoi
+	for (int i = 0; i < nDisks; i++)
+	{
+		Color c;
+		if (randomColors)
+			c = Color(rCol(rng), rCol(rng), rCol(rng));
+		else
+			c = Color(55 + cC * i, 0, 0);
+		disks[i].disk = Rect(0, 0, firstW - i * widthChange, firstH - i * heightChange, c);
+	}
+}
+
+void Hanoi::SetupDoubleHanoi()
+{
+	for (int j = 0; j < nDisks; j++)
+	{
+		pegs[0].highestPegI.push_back(j);
+	}
+}
+
+void Hanoi::SetupMergeHanoi()
+{
+	for (int j = 0; j < nDisks; j++)
+	{
+		pegs[0].highestPegI.push_back(j);
+		j++;
+		pegs[1].highestPegI.push_back(j);
+	}
+}
+
+void Hanoi::SetupSplitHanoi()
+{
+	SetupDoubleHanoi();
+}
+
+void Hanoi::SetupBaseSwapHanoi()
+{
+	int i = 0;
+	pegs[1].highestPegI.push_back(i++);
+	pegs[0].highestPegI.push_back(i++);
+	for (int j = 2; j < nDisks; j++)
+	{
+		pegs[0].highestPegI.push_back(j);
+		j++;
+		pegs[1].highestPegI.push_back(j);
+	}
+}
+
+void Hanoi::SetupEasyBicolorHanoi()
+{
+	bool fLeft = false;
+	for (int j = 0; j < nDisks; j++)
+	{
+		if (fLeft)
+		{
+			pegs[0].highestPegI.push_back(j);
+			j++;
+			pegs[1].highestPegI.push_back(j);
+			fLeft = false;
+		}
+		else
+		{
+			pegs[1].highestPegI.push_back(j);
+			j++;
+			pegs[0].highestPegI.push_back(j);
+			fLeft = true;
+		}
+	}
+}
+
+void Hanoi::SetupBicolorHanoi()
+{
+	SetupEasyBicolorHanoi();
+}
+
 void Hanoi::LoadStep()
 {
 	for (int i = 0; i < 3; i++)
@@ -78,12 +292,12 @@ void Hanoi::LoadStep()
 	}
 }
 
-int Hanoi::GetCS()
+int Hanoi::GetCS() const
 {
 	return currentStep;
 }
 
-int Hanoi::GetMaxStep()
+int Hanoi::GetMaxStep() const
 {
 	return steps.size() - 1;
 }
@@ -114,20 +328,4 @@ void Hanoi::Draw(Graphics& gfx) const
 void Hanoi::Disk::Init(const Rect& sRect)
 {
 	disk = sRect;
-}
-
-void Hanoi::Peg::RefreshA()
-{
-	for (int i = 0; i < nDisks - 1; i++)
-	{
-		highestPegI[i] = highestPegI[i + 1];
-	}
-}
-
-void Hanoi::Peg::RefreshB()
-{
-	for (int i = nDisks - 1; i > 0; i--)
-	{
-		highestPegI[i] = highestPegI[i - 1];
-	}
 }
--- Engine/Hanoi.h
+++ Engine/Hanoi.h
@@ -7,11 +7,26 @@
 #include "Graphics.h"
 
 class Hanoi
-{
-	//number of disks (in bicolor modes it covers col0 + col1)
-	static constexpr int nDisks = 7;
+{	
+	/* Variables: */
+	/************************************************************/
+	//Number of disks (in bicolor modes it covers col0 + col1)
+	static constexpr int nDisks = 8;
 	//
-
+	const int gap = 20; //gap between pegs
+	const int topGap = 250; //gap between the top of the screen, and the top of the tower
+	const int widthChange = 20;
+	const int heightChange = 5;
+	bool randomColors = false; //only in normhanoi
+	//Modes:
+	bool normHanoi = false;
+	bool doubleHanoi = false;
+	bool mergeHanoi = false;
+	bool splitHanoi = false;
+	bool baseSwapHanoi = false;
+	bool easyBicolorHanoi = false;
+	bool bicolorHanoi = true;
+	/************************************************************/
 private:
 	class Disk
 	{
@@ -24,9 +39,6 @@ private:
 	class Peg
 	{
 	public:
-		void RefreshA();
-		void RefreshB();
-	public:
 		int x;
 		int index;
 		std::vector <int> highestPegI;
@@ -36,25 +48,33 @@ private:
 public:
 	Hanoi(Graphics& gfx);
 	void LoadStep();
-	int GetCS();
-	int GetMaxStep();
+	int GetCS() const;
+	int GetMaxStep() const;
 	void ChangeCurrentStep(int c);
 	void Draw(Graphics& gfx) const;
 private:
 	void FromAToB(Peg& A, Peg& B);
+	//setup functions:
+	void BicolorDiskSetup(const int firstW, const int firstH);
+	void SetupNormHanoi(const int firstW, const int firstH);
+	void SetupDoubleHanoi();
+	void SetupMergeHanoi();
+	void SetupSplitHanoi();
+	void SetupBaseSwapHanoi();
+	void SetupEasyBicolorHanoi();
+	void SetupBicolorHanoi();
+	//logic functions
 	void NormalHanoi(int n, Peg& A, Peg& B, Peg& Via);
-
+	void EnhancedDoubleHanoi(int n, Peg& A, Peg& B, Peg& Via);
+	void DoubleHanoi(int n, Peg& A, Peg& B, Peg& Via);
+	void MergeHanoi(int n, Peg& A, Peg& B, Peg& Via);
+	void SplitHanoi(int n, Peg& A, Peg& B, Peg& Via);
+	void BaseSwapHanoi(int n, Peg& A, Peg& B, Peg& Via);
+	void EasyBicolorHanoi(int n, Peg& A, Peg& B, Peg& Via);
+	void BicolorHanoi(int n, Peg& A, Peg& B, Peg& Via);
 private:
 	std::vector <std::array<int, 2>> steps;
 	int currentStep = -1;
 	Disk disks[nDisks];
 	Peg pegs[3];
-
-	//changeable variables
-	const int gap = 20; //gap between pegs
-	const int topGap = 250; //gap between the top of the screen, and the top of the tower
-	const int widthChange = 20;
-	const int heightChange = 5;
-	bool randomColors = false;
-	//
 };
